267db03b6ff5fdd3ecc7e2c97fc1e040a20f6506
Cody Fincher <cody.fincher@gmail.com>
2023-12-13 12:09:02 -0600

    fix: make name singular

--- examples/vue/app.py
+++ examples/vue/app.py
@@ -13,7 +13,7 @@ vite = VitePlugin(
     config=ViteConfig(
         bundle_dir=Path(here / "web" / "public"),
         resource_dir=Path(here / "web" / "resources"),
-        templates_dir=Path(here / "web" / "templates"),
+        template_dir=Path(here / "web" / "templates"),
         hot_reload=True,
         port=3006,
     ),
