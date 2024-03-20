8a34375ddb9f62416011cfc421e370ebeb15a3d3
Cody Fincher <cody.fincher@gmail.com>
2024-03-19 21:29:34 -0500

    feat: don't use `NoneType`

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from types import NoneType
 
 from litestar_vite.config import ViteConfig
 
@@ -8,6 +7,6 @@ def test_default_vite_config() -> None:
     config = ViteConfig()
     assert isinstance(config.bundle_dir, Path)
     assert isinstance(config.public_dir, Path)
-    assert isinstance(config.ssr_output_dir, NoneType)
+    assert config.ssr_output_dir is None
     assert isinstance(config.resource_dir, Path)
-    assert isinstance(config.root_dir, NoneType)
+    assert config.root_dir is None
