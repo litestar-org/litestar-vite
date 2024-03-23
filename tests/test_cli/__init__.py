10cbd03b1d4f77d42d611132301de9e9b5a5ef44
Cody Fincher <cody.fincher@gmail.com>
2024-03-23 14:36:58 -0500

    chore: updated tests

--- tests/test_cli/__init__.py
+++ tests/test_cli/__init__.py
@@ -1,4 +1,20 @@
-APP_FILE_CONTENT = """
+from __future__ import annotations
+
+APP_DEFAULT_CONFIG_FILE_CONTENT = """
 from litestar import Litestar
-app = Litestar([])
+
+from litestar_vite import VitePlugin
+
+app = Litestar(plugins=[VitePlugin()])
+"""
+
+
+APP_BASIC_NO_ROUTES_FILE_CONTENT = """
+from litestar import Litestar
+
+from litestar_vite import ViteConfig, VitePlugin
+
+vite = VitePlugin(config=ViteConfig(template_dir="{template_dir!s}"))
+
+app = Litestar(plugins=[vite])
 """
