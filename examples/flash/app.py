baf78310470e4976a26cc580f6c2887b66e6a831
Cody Fincher <cody.fincher@gmail.com>
2024-05-26 18:52:19 +0000

    feat: easier integration with the `flash` plugin

---
+++ examples/flash/app.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from litestar import Controller, Litestar, get, Request
+from litestar.response import Template
+from litestar.plugins.flash import FlashConfig, FlashPlugin, flash
+
+from litestar_vite import ViteConfig, VitePlugin
+
+here = Path(__file__).parent
+
+
+class WebController(Controller):
+    """Web Controller."""
+
+    opt = {"exclude_from_auth": True}
+    include_in_schema = False
+
+    @get("/")
+    async def index(self, request: Request) -> Template:
+        """Serve site root."""
+        flash(request, "Oh no! I've been flashed!", category="error")
+
+        return Template(template_name="index.html.j2")
+
+
+vite = VitePlugin(
+    config=ViteConfig(
+        hot_reload=True,
+        port=3006,
+        use_server_lifespan=True,
+        dev_mode=True,
+    ),
+)
+flasher = FlashPlugin(config=FlashConfig(template_config=vite.template_config))
+
+app = Litestar(plugins=[vite,flasher], route_handlers=[WebController])
