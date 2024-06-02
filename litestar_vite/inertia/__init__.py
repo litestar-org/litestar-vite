c957b5ffb5b09b7797d16ec61cecad1f87e48dd4
Cody Fincher <cody.fincher@gmail.com>
2024-06-02 22:22:58 +0000

    fix: imports

--- litestar_vite/inertia/__init__.py
+++ litestar_vite/inertia/__init__.py
@@ -1,14 +1,13 @@
 from .config import InertiaConfig
 from .plugin import InertiaPlugin
 from .request import InertiaDetails, InertiaHeaders, InertiaRequest
-from .response import InertiaJSON, InertiaTemplate
+from .response import InertiaResponse
 
 __all__ = (
     "InertiaConfig",
     "InertiaDetails",
     "InertiaHeaders",
     "InertiaRequest",
-    "InertiaTemplate",
-    "InertiaJSON",
+    "InertiaResponse",
     "InertiaPlugin",
 )
