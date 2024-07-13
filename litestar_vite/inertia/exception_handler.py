f3940a9bc0631cd9f6e663fdb70d7cdeea7233c4
Cody Fincher <cody.fincher@gmail.com>
2024-07-13 23:14:45 +0000

    fix: adjust logic

--- litestar_vite/inertia/exception_handler.py
+++ litestar_vite/inertia/exception_handler.py
@@ -55,27 +55,26 @@ def exception_to_http_response(request: Request[UserT, AuthT, StateT], exc: Exce
     is_inertia = getattr(request, "is_inertia", False)
     status_code = getattr(exc, "status_code", HTTP_500_INTERNAL_SERVER_ERROR)
     preferred_type = MediaType.HTML if inertia_enabled and not is_inertia else MediaType.JSON
+    detail = getattr(exc, "detail", "")  # litestar exceptions
+    extras = getattr(exc, "extra", "")  # msgspec exceptions
     content = {"status_code": status_code, "detail": getattr(exc, "detail", "")}
-    extra = getattr(exc, "extra", "")
-    if extra:
-        content.update({"extra": extra})
-    if is_inertia:
-        detail = getattr(exc, "detail", "")  # litestar exceptions
-        extras = getattr(exc, "extra", "")  # msgspec exceptions
-        if extras and len(extras) >= 1:
-            message = extras[0]
-            error_detail = cast("str", message.get("message", detail))  # type: ignore[union-attr] # pyright: ignore[reportUnknownMemberType,reportUnknownArgumentType]
-            match = FIELD_ERR_RE.search(error_detail)
-            field = match.group(1) if match else cast("str", message.get("key", ""))  # type: ignore[union-attr] # pyright: ignore[reportUnknownMemberType,reportUnknownArgumentType]
-            if isinstance(message, dict):
-                error(request, field, error_detail)
-        if status_code in {HTTP_422_UNPROCESSABLE_ENTITY, HTTP_400_BAD_REQUEST}:
-            # redirect to the original page and flash the errors
-            return Redirect(
-                path=request.headers["Referer"],
-                status_code=HTTP_307_TEMPORARY_REDIRECT if request.method == "GET" else HTTP_303_SEE_OTHER,
-                cookies=request.cookies,
-            )
+    if extras:
+        content.update({"extra": extras})
+
+    if extras and len(extras) >= 1:
+        message = extras[0]
+        error_detail = cast("str", message.get("message", detail))  # type: ignore[union-attr] # pyright: ignore[reportUnknownMemberType,reportUnknownArgumentType]
+        match = FIELD_ERR_RE.search(error_detail)
+        field = match.group(1) if match else cast("str", message.get("key", ""))  # type: ignore[union-attr] # pyright: ignore[reportUnknownMemberType,reportUnknownArgumentType]
+        if isinstance(message, dict):
+            error(request, field, error_detail)
+    if status_code in {HTTP_422_UNPROCESSABLE_ENTITY, HTTP_400_BAD_REQUEST}:
+        # redirect to the original page and flash the errors
+        return Redirect(
+            path=request.headers["Referer"],
+            status_code=HTTP_307_TEMPORARY_REDIRECT if request.method == "GET" else HTTP_303_SEE_OTHER,
+            cookies=request.cookies,
+        )
 
     return InertiaResponse[Any](
         media_type=preferred_type,
