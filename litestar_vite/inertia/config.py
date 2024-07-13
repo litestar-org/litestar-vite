768994088b2e10852bf68916ca1de00c6c71cecf
Cody Fincher <cody.fincher@gmail.com>
2024-07-13 23:07:03 +0000

    feat: prep for exclude support

--- litestar_vite/inertia/config.py
+++ litestar_vite/inertia/config.py
@@ -16,3 +16,5 @@ class InertiaConfig:
     """
     component_opt_key: str = "component"
     """An identifier to use on routes to get the inertia component to render."""
+    exclude_from_js_routes_key: str = "exclude_from_routes"
+    """An identifier to use on routes to exclude a route from the generated routes typescript file."""
