42ffc3fd296503a7514199b20207e74595172cc8
Cody Fincher <204685+cofin@users.noreply.github.com>
2024-07-26 21:18:06 -0500

    feat: better handle errors (#37)
    * feat: better handle errors
    
    * feat: updated tests

--- litestar_vite/inertia/exception_handler.py
+++ litestar_vite/inertia/exception_handler.py
@@ -1,4 +1,3 @@
-import contextlib
 import re
 from typing import TYPE_CHECKING, Any, cast
 
@@ -44,7 +43,7 @@ class _HTTPConflictException(HTTPException):
     status_code = HTTP_409_CONFLICT
 
 
-def exception_to_http_response(request: Request[UserT, AuthT, StateT], exc: Exception) -> Response[Any]:  # noqa: PLR0911
+def exception_to_http_response(request: Request[UserT, AuthT, StateT], exc: Exception) -> Response[Any]:
     """Handler for all exceptions subclassed from HTTPException."""
     inertia_enabled = getattr(request, "inertia_enabled", False) or getattr(request, "is_inertia", False)
     if isinstance(exc, NotFoundError):
@@ -57,17 +56,25 @@ def exception_to_http_response(request: Request[UserT, AuthT, StateT], exc: Exce
         if request.app.debug and http_exc not in (PermissionDeniedException, NotFoundError):
             return cast("Response[Any]", create_debug_response(request, exc))
         return cast("Response[Any]", create_exception_response(request, http_exc(detail=str(exc.__cause__))))
+    return create_inertia_exception_response(request, exc)
+
+
+def create_inertia_exception_response(request: Request[UserT, AuthT, StateT], exc: Exception) -> Response[Any]:
+    """Create the inertia exception response"""
     is_inertia = getattr(request, "is_inertia", False)
     status_code = getattr(exc, "status_code", HTTP_500_INTERNAL_SERVER_ERROR)
-    preferred_type = MediaType.HTML if inertia_enabled and not is_inertia else MediaType.JSON
+    preferred_type = MediaType.HTML if not is_inertia else MediaType.JSON
     detail = getattr(exc, "detail", "")  # litestar exceptions
     extras = getattr(exc, "extra", "")  # msgspec exceptions
     content = {"status_code": status_code, "message": getattr(exc, "detail", "")}
     inertia_plugin = cast("InertiaPlugin", request.app.plugins.get("InertiaPlugin"))
     if extras:
         content.update({"extra": extras})
-    with contextlib.suppress(Exception):
+    try:
         flash(request, detail, category="error")
+    except Exception as flash_exc:
+        msg = f"Failed to set the `flash` session state.  Reason: {flash_exc.__cause__!s}"
+        request.logger.exception(msg)
     if extras and len(extras) >= 1:
         message = extras[0]
         default_field = f"root.{message.get('key')}" if message.get("key", None) is not None else "root"  # type: ignore
@@ -75,8 +82,7 @@ def exception_to_http_response(request: Request[UserT, AuthT, StateT], exc: Exce
         match = FIELD_ERR_RE.search(error_detail)
         field = match.group(1) if match else default_field
         if isinstance(message, dict):
-            with contextlib.suppress(Exception):
-                error(request, field, error_detail)
+            error(request, field, error_detail)
     if status_code in {HTTP_422_UNPROCESSABLE_ENTITY, HTTP_400_BAD_REQUEST} or isinstance(
         exc,
         PermissionDeniedException,
@@ -85,15 +91,15 @@ def exception_to_http_response(request: Request[UserT, AuthT, StateT], exc: Exce
     if isinstance(exc, PermissionDeniedException):
         return InertiaBack(request)
     if status_code == HTTP_401_UNAUTHORIZED or isinstance(exc, NotAuthorizedException):
-        redirect_to = (
+        if (
             inertia_plugin.config.redirect_unauthorized_to is not None
             and str(request.url) != inertia_plugin.config.redirect_unauthorized_to
-        )
-        if redirect_to:
-            return InertiaRedirect(request, redirect_to=cast("str", inertia_plugin.config.redirect_unauthorized_to))
-        return InertiaBack(request)
-    return InertiaResponse[Any](
-        media_type=preferred_type,
-        content=content,
-        status_code=status_code,
-    )
+        ):
+            return InertiaRedirect(request, redirect_to=inertia_plugin.config.redirect_unauthorized_to)
+        if str(request.url) != inertia_plugin.config.redirect_unauthorized_to:
+            return InertiaResponse[Any](
+                media_type=preferred_type,
+                content=content,
+                status_code=status_code,
+            )
+    return InertiaBack(request)
