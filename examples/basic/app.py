7948d57a071c56cfc101066ca506f016950b31e6
Cody Fincher <204685+cofin@users.noreply.github.com>
2024-12-08 20:40:51 -0600

    feat: improved process management (#59)

--- examples/basic/app.py
+++ examples/basic/app.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from pathlib import Path
 
 from litestar import Controller, Litestar, get
+from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.response import Template
+from litestar.template.config import TemplateConfig
 
 from litestar_vite import ViteConfig, VitePlugin
 
@@ -22,6 +24,7 @@ class WebController(Controller):
         return Template(template_name="index.html.j2")
 
 
+templates = TemplateConfig(engine=JinjaTemplateEngine(directory=here / "templates"))
 vite = VitePlugin(
     config=ViteConfig(
         hot_reload=True,
@@ -30,4 +33,4 @@ vite = VitePlugin(
         dev_mode=True,
     ),
 )
-app = Litestar(plugins=[vite], route_handlers=[WebController])
+app = Litestar(plugins=[vite], route_handlers=[WebController], template_config=templates)
--- examples/flash/app.py
+++ examples/flash/app.py
@@ -4,10 +4,12 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 from litestar import Controller, Litestar, Request, get
+from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.middleware.session.server_side import ServerSideSessionConfig
 from litestar.plugins.flash import FlashConfig, FlashPlugin, flash  # pyright: ignore[reportUnknownVariableType]
 from litestar.response import Template
 from litestar.stores.memory import MemoryStore
+from litestar.template.config import TemplateConfig
 
 from litestar_vite import ViteConfig, VitePlugin
 from litestar_vite.inertia import InertiaConfig, InertiaPlugin
@@ -32,21 +34,22 @@ class WebController(Controller):
         return Template(template_name="index.html.j2")
 
 
+templates = TemplateConfig(engine=JinjaTemplateEngine(directory=here / "templates"))
 vite = VitePlugin(
     config=ViteConfig(
         hot_reload=True,
         port=3006,
         use_server_lifespan=True,
         dev_mode=True,
-        template_dir="templates/",
     ),
 )
 inertia = InertiaPlugin(config=InertiaConfig())
-flasher = FlashPlugin(config=FlashConfig(template_config=vite.template_config))
+flasher = FlashPlugin(config=FlashConfig(template_config=templates))
 
 app = Litestar(
     plugins=[vite, flasher],
     route_handlers=[WebController],
     middleware=[ServerSideSessionConfig().middleware],
+    template_config=templates,
     stores={"sessions": MemoryStore()},
 )
