f035af540e12cb70d7b546d18ce4b843df5c13c5
Cody Fincher <cody@litestar.dev>
2025-03-27 03:07:59 +0000

    chore(release): bump to v0.13.1

--- src/py/litestar_vite/commands.py
+++ src/py/litestar_vite/commands.py
@@ -15,7 +15,7 @@ DEFAULT_RESOURCES: "set[str]" = {"styles.css.j2", "main.ts.j2"}
 DEFAULT_DEV_DEPENDENCIES: "dict[str, str]" = {
     "typescript": "^5.7.2",
     "vite": "^6.0.6",
-    "litestar-vite-plugin": "^0.13.0",
+    "litestar-vite-plugin": "^0.13.1",
     "@types/node": "^22.10.2",
 }
 DEFAULT_DEPENDENCIES: "dict[str, str]" = {"axios": "^1.7.9"}
